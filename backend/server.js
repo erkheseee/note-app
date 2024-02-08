b703bf802bf504757fccc506d6b4967f082b2c7a
erkhes01 <106813277+erkheseee@users.noreply.github.com>
2024-02-08 13:16:47 +0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,34 +6,12 @@ const folderRoute = require('./routes/folder');
 
 const app = express();
 
-// Set middleware of CORS 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
 const port = process.env.PORT || 4000;
 const cors = require('cors');
 
 //middleware
 app.use(cors({
-    origin: ["http://localhost:4000", "https://note-app-ycdm.onrender.com", "https://note-app-coral-eight.vercel.app", "http://localhost:5173"],
+    origin: ["https://note-app-coral-eight.vercel.app", "http://localhost:5173"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 }));
 app.use(express.json());
